a6e71327c874155412055dad7da28c328576d898
Enes Duhan Bulut <dev.duhanbulut@gmail.com>
2023-08-16 00:49:33 +0300

    improve performance(only test)

--- src/main/kotlin/com/example/demo/SegmentationContoller.kt
+++ src/main/kotlin/com/example/demo/SegmentationContoller.kt
@@ -1,27 +1,31 @@
 package com.example.demo
 
+import com.google.gson.Gson
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.pytorch.IValue
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import com.google.gson.Gson
-import java.awt.image.BufferedImage
-import java.util.Base64
 
 @RestController
 @RequestMapping("/api/classifier")
 class ClassifierController {
-	val gson = Gson()
+    private val gson = Gson()
+
     @PostMapping("/predict")
     fun predict(@RequestBody segmentationRequest: SegmentationRequest): ResponseEntity<*> {
         require(segmentationRequest.modelName.isNotBlank()) { "Model name is required" }
         require(segmentationRequest.image.isNotBlank()) { "Image is required" }
+
         val module = ModelRepository.readModelFromResources(segmentationRequest.modelName)
         val imageSize = module.second!!.size(segmentationRequest.modelName)
         val baseImage = base64ToImage(segmentationRequest.image, imageSize, imageSize)
         require(baseImage != null) { "Image is not valid" }
+
         val normMeanRGB = floatArrayOf(0.0f, 0.0f, 0.0f)
         val normStdRGB = floatArrayOf(1.0f, 1.0f, 1.0f)
         val input = TensorImageUtils.bitmapToFloat32Tensor(
@@ -29,76 +33,36 @@ class ClassifierController {
             normMeanRGB,
             normStdRGB
         )
+
         val outTensors: Array<IValue> = module.second!!.forward(IValue.from(input)).toTuple()
-        val compute =
-            Compute(listOf(*outTensors), module.second!!.size(segmentationRequest.modelName))
-        val pre1: BufferedImage = createScaledBufferedImage(
-            compute.getmask(2),
-            baseImage.width,
-            baseImage.height,
-        )
-        val pre2 = createScaledBufferedImage(
-            compute.getdepth(2),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap1 = createScaledBufferedImage(
-            compute.get_q_mask(0),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap2 = createScaledBufferedImage(
-            compute.get_q_mask(1),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap3 = createScaledBufferedImage(
-            compute.get_q_mask(2),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap4 = createScaledBufferedImage(
-            compute.get_q_mask(3),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap5 = createScaledBufferedImage(
-            compute.get_q_depth(0),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap6 = createScaledBufferedImage(
-            compute.get_q_depth(1),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap7 = createScaledBufferedImage(
-            compute.get_q_depth(2),
-            baseImage.width,
-            baseImage.height,
-        )
-        val transferredBitmap8 = createScaledBufferedImage(
-            compute.get_q_depth(3),
-            baseImage.width,
-            baseImage.height,
-        )
-        val encoder = Base64.getEncoder()
-        // Return the prediction as a JSON string
-        return ResponseEntity.ok<Any>(
-            gson.toJson(SegmentationResponse(
-                listOf(
-                    transferredBitmap1.toByteArray(),
-                    transferredBitmap2.toByteArray(),
-                    transferredBitmap3.toByteArray(),
-                    transferredBitmap4.toByteArray(),
-                    transferredBitmap5.toByteArray(),
-                    transferredBitmap6.toByteArray(),
-                    transferredBitmap7.toByteArray(),
-                    transferredBitmap8.toByteArray(),
-                )
-            ))
-        )
+        val compute = Compute(listOf(*outTensors), module.second!!.size(segmentationRequest.modelName))
+
+        val transferredBitmaps = runBlocking {
+            val transferJobs = (0 until 8).map { index ->
+                async {
+                    when (index) {
+                        in 0..3 -> createScaledBufferedImage(
+                            compute.get_q_mask(index),
+                            baseImage.width,
+                            baseImage.height
+                        )
+
+                        else -> createScaledBufferedImage(
+                            compute.get_q_depth(index - 4),
+                            baseImage.width,
+                            baseImage.height
+                        )
+                    }
+                }
+            }
+            transferJobs.awaitAll()
+        }
+
+        val encodedBitmaps = transferredBitmaps.map { bitmap ->
+            bitmap.toByteArray()
+        }
+
+        val response = SegmentationResponse(encodedBitmaps)
+        return ResponseEntity.ok<Any>(gson.toJson(response))
     }
-
-
 }
